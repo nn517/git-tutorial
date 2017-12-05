cfa968230ad666668a7cc79352e8547f95b950fa
Nishizaka <j1428@matsue-ct.jp>
2017-12-05 09:37:56 +0900

    directory bin

---
+++ bin/fizzbuzz.rb
@@ -0,0 +1,18 @@
+
+(1..100).each{|i|
+	if i%3==0&&i%5==0 then 
+		print("fizzbuzz\n");
+
+
+	elsif i%3==0 then
+		print("Fizz\n");
+
+	elsif i%5==0 then
+		print("Buzz\n");
+	
+	else
+		printf("%d\n",i);
+	
+	end
+
+}
